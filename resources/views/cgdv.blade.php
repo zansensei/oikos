33fd5829a8780861b455373447030a59dc08e955
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-02-26 16:48:13 +0100

    Ajout page CGDV

---
+++ resources/views/cgdv.blade.php
@@ -0,0 +1,73 @@
+@extends ("layout")
+
+@section ("content")
+
+<div>
+<strong><h1>Article 1'Objet</h1>></strong>
+
+<p>Les conditions générales de ventes décrites ci-après détaillent les droits et obligations de l'entreprise Oikos et de ses clients dans le cadre de la vente des marchandises suivantes produits technologique à but écologique.
+
+    Toute prestation accomplie par la société Oikos implique l'adhésion sans réserve de l'acheteur aux présentes conditions générales de vente.</p><br>
+
+
+<strong><h2>Article 2 Présentation des produits</h2></strong><br>
+
+<p>Les caractéristiques des produits proposés à la vente sont présentées dans la rubrique " Catalogue " de notre site. Les photographies n'entrent pas dans le champ contractuel. La responsabilité de la société . . . . . . ne peut être engagée si des erreurs s'y sont introduites. Tous les textes et images présentés sur le site de la société . . . . . . sont réservés, pour le monde entier, au titre des droits d'auteur et de propriété intellectuelle; leur reproduction, même partielle, est strictement interdite.</p><br>
+<strong><h2>Article 3 Durée de validité des offres de vente</h2></strong><br>
+
+<p>Les produits sont proposés à la vente jusqu'à épuisement du stock. En cas de commande d'un produit devenu indisponible, le client sera informé de cette indisponibilité, dans les meilleurs délais, par courrier électronique ou par courrier postal.</p><br>
+<strong><h2>Article 4 Prix des produits</h2></strong><br>
+
+<p>La rubrique " Catalogue " de notre site indique les prix en euros toutes taxes comprises, hors frais de port. Le montant de la TVA est précisé lors de la sélection d'un produit par le client et les frais de port apparaissent sur l'écran à la fin de la sélection des différents produits par le client.
+
+La société Oikos se réserve le droit de modifier ses prix à tout moment mais les produits commandés sont facturés au prix en vigueur lors de l'enregistrement de la commande.
+
+Les tarifs proposés comprennent les rabais et ristournes que l'entreprise Oikos serait amenée à octroyer compte tenu de ses résultats ou de la prise en charge par l'acheteur de certaines prestations.
+
+    Aucun escompte ne sera accordé en cas de paiement anticipé.</p><br>
+    </h2><strong>Article 5 Commande</h2></strong><br>
+
+<p>Le client valide sa commande lorsqu'il active le lien " Confirmez votre commande " en bas de la page " Récapitulatif de votre commande " après avoir accepté les présentes conditions de vente. Avant cette validation, il est systématiquement proposé au client de vérifier chacun des éléments de sa commande; il peut ainsi corriger ses erreurs éventuelles.
+
+La société Oikos confirme la commande par courrier électronique; cette information reprend notamment tous les éléments de la commande et le droit de rétractation du client.
+
+    Les données enregistrées par la société Oikos constituent la preuve de la nature, du contenu et de la date de la commande. Celle-ci est archivée par la société Oikos dans les conditions et les délais légaux; le client peut accéder à cet archivage en contactant le service Relations Clients.</p><br>
+<h2><strong>Article 6 Modalités de paiement</h2></strong><br>
+
+<p>Le règlement des commandes s'effectue par chèque ou carte bancaire.
+
+    Lors de l'enregistrement de la commande, l'acheteur devra verser un acompte de 10 % du montant global de la facture, le solde devant être payé à réception des marchandises</p><br>
+<h2><strong>Article 7 Délai de rétractation</h2></strong><br>
+
+<p>L'Acheteur dispose d'un délai de quatorze jours francs, à compter de la réception des produits, pour exercer son droit de rétractation sans avoir à justifier de motifs ni à payer de pénalités, à l'exception, le cas échéant, des frais de retour. Si le délai de quatorze jours vient à expirer un samedi, un dimanche ou un jour férié ou chômé, il est prorogé jusqu'au premier jour ouvrable suivant.
+
+En cas d'exercice du droit de rétractation, la société rembourse l'Acheteur de la totalité des sommes versées, dans les meilleurs délais et au plus tard dans les quatorze jours suivant la date à laquelle ce droit a été exercé.
+Article 8 Retard de paiement (clause applicable aux professionnels)
+
+En cas de défaut de paiement total ou partiel des marchandises livrées au jour de la réception, l'acheteur doit verser à l'entreprise Oikos une pénalité de retard égale à une fois et demi le taux de l'intérêt légal.
+
+Le taux de l'intérêt légal retenu est celui en vigueur au jour de la livraison des marchandises.
+
+Cette pénalité est calculée sur le montant hors taxes de la somme restant due, et court à compter de la date d'échéance du prix sans qu'aucune mise en demeure préalable ne soit nécessaire.
+
+Conformément aux articles 441-6 c. com. et D. 441-5 c. com., tout retard de paiement entraîne de plein droit, outre les pénalités de retard, une obligation pour le débiteur de payer une indemnité forfaitaire de 40€ pour frais de recouvrement.
+
+Une indemnité complémentaire pourra être réclamée, sur justificatifs, lorsque les frais de recouvrement exposés sont supérieurs au montant de l'indemnité forfaitaire.</p><br>
+<h2><strong>Article 9 Livraison</h2></strong><br>
+
+<p>Tout produit est livré sans garantie quant aux délais, exception faite des livraisons aux particuliers. La date limite de livraison varie suivant leur adresse. Elle est fixée, pour une adresse en France métropolitaine, au jour du paiement + 8 jours et, pour les autres destinations, au jour du paiement + 1 mois.</p><br>
+
+<h2><strong>Article 10 Relations clients - Service après-vente</h2></strong><br>
+
+<p>Pour toute information, question ou réclamation, le client peut s'adresser du lundi au vendredi, de 9 h à 18 h au service Relations Clients de la société</p><br>
+
+    <li>Adresse:oikos@gmail.com</li><br>
+
+    <li>Tél: 04 50 12 34 56</li><br>
+
+    <li>Fax: 04 50 64 63 62</li><br>
+
+    <li>e-mail: oikos@gmail.com </li><br>
+</div>
+
+    @endsection
