6ef43cb30f1961e57ed4c716aceb3321a2e2dea9
Kevin <kevin.ceriati@le-campus-numerique.fr>
2019-03-01 16:14:23 +0100

    rename ReviewController

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-class FormsController extends BaseController
+class ReviewController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     /**
